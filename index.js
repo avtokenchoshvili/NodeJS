47567ede3095c5aad75c8f10d832846ffe2f9e49
avto kenchoshvili <Tandila.92@gmail.com>
2023-02-18 17:02:10 +0400

    back

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+require('./start/routes.start')(app);
+require('./start/db.start')();
+
+if (!process.env.PRIVATE_KEY) {
+    process.exit(1);
+}
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ models/challenges.model.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Challenge = mongoose.model('challanges', new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 255,
+    },
+    smallDescr: {
+        type: String,
+        required: true,
+    },
+    descr: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    level: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: Array,
+        required: true,
+    }
+}));
+
+const schema = Joi.object({
+    _id: Joi.optional().allow(''),
+    title: Joi.string().max(255).required(),
+    smallDescr: Joi.string().required(),
+    descr: Joi.string().required(),
+    img: Joi.string().required(),
+    level: Joi.string().required(),
+    tags: Joi.array().required(),
+});
+
+module.exports.Challange = Challenge;
+module.exports.ChallengeValidation = schema;
